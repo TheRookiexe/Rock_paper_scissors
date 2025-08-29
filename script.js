f73a4c0a4edc7f74571c267910e3e91257943303
TheRookiexe <mishrxa36@gmail.com>
2025-08-29 13:29:03 +0530

    Optimized getComputerChoice function.

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function getComputerChoice(){
-    comp_choice=Math.floor(Math.random()*3);
-    let R="Rock";
-    let P="Paper";
-    let S="Scissors";
-    // console.log(comp_choice);
-    if (comp_choice==0){
-        return R;
-    }else if(comp_choice==1){
-        return P;
-    }else if(comp_choice==2){
-        return S;
-    }
+    // comp_choice=Math.floor(Math.random()*3);
+    // let R="Rock";
+    // let P="Paper";
+    // let S="Scissors";
+    // if (comp_choice==0){
+    //     return R;
+    // }else if(comp_choice==1){
+    //     return P;
+    // }else if(comp_choice==2){
+    //     return S;
+    // }
+    /---optimized---/ 
+    const comp_choice=["Rock", "Paper", "Scissors"];
+    return comp_choice[Math.floor(Math.random()*comp_choice.length)];
 }
+console.log(getComputerChoice())
