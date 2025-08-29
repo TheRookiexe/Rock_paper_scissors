723bd4818c60465ae060e1b3f5c346374ba65290
TheRookiexe <mishrxa36@gmail.com>
2025-08-29 13:51:03 +0530

    Code cleaned

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 function getComputerChoice(){
-    // comp_choice=Math.floor(Math.random()*3);
-    // let R="Rock";
-    // let P="Paper";
-    // let S="Scissors";
-    // if (comp_choice==0){
-    //     return R;
-    // }else if(comp_choice==1){
-    //     return P;
-    // }else if(comp_choice==2){
-    //     return S;
-    // }
-    /---optimized---/ 
     const comp_choice=["rock", "paper", "scissors"];
     return comp_choice[Math.floor(Math.random()*comp_choice.length)];
 }
