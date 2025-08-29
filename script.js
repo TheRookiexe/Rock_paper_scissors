dc2de842c40cc9569195684059834f456898534b
TheRookiexe <mishrxa36@gmail.com>
2025-08-29 13:23:29 +0530

    Computer's choice fuction created and working.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('hello, world!')
+function getComputerChoice(){
+    comp_choice=Math.floor(Math.random()*3);
+    let R="Rock";
+    let P="Paper";
+    let S="Scissors";
+    // console.log(comp_choice);
+    if (comp_choice==0){
+        return R;
+    }else if(comp_choice==1){
+        return P;
+    }else if(comp_choice==2){
+        return S;
+    }
+}
