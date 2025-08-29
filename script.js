5e71713cc429cbd02da7c1db670d2525ed13d5b7
TheRookiexe <mishrxa36@gmail.com>
2025-08-30 00:35:03 +0530

    Cleaned the code.
    made some final changes and all working

--- script.js
+++ script.js
@@ -10,13 +10,6 @@ function getComputerChoice(){
     const comp_choice=["rock", "paper", "scissors"];
     return comp_choice[Math.floor(Math.random()*comp_choice.length)];
 }
-// console.log(getComputerChoice())
-
-// function getHumanChoice(){
-//     let users_Choice=prompt("Enter your choice (Rock, Paper, Scissors): ").toLowerCase();
-//     return users_Choice;
-// }
-// console.log(getHumanChoice())
 
 let humanScore=0;
 let computerScore=0;
